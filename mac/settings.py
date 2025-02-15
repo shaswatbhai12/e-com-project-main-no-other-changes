3514883e09f1b01c2845cb8c0550efc1e34ec870
shaswat <shaswatkumar9868@gmail.com>
2025-02-15 18:58:10 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9h_#wy*6)%#ug3-uv@7xlryan5a36rqe^j5a$-i0@fo9szu=%n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-app-9dfw.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-app.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
